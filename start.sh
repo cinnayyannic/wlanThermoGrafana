900d04edf9f039f4e49c1084b0b79fd530269468
Yannic <Yannic@Yannic-Thinkpad>
2020-05-02 22:08:58 +0200

    up

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mv /deploy/grafana/wlanthermo.json /var/lib/grafana/dashboards/
-mv /deploy/mosquitto/mosquitto.passwd /var/lib/mosquitto/
+# deploy config files to shared volumes
+#mkdir -p /var/lib/grafana/dashboards/
+#mv /deploy/grafana/wlanthermo.json /var/lib/grafana/dashboards/
+#mkdir -p /var/lib/mosquitto/
+#mv /deploy/mosquitto/mosquitto.passwd /var/lib/mosquitto/
 
 exec /usr/bin/supervisord
